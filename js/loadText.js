decfe21da2080bceff74a030344e9d747e8788db
1337Zero <julius.schoenhut@gmx.net>
2014-11-15 20:46:59 +0100

    Added Jquery-Script to load Content from a file

---
+++ js/loadText.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+$(".active").click(function(){
+$(".starter-template").load("text/latest.html");
+});
+
+$(".contact").click(function(){
+$(".starter-template").load("text/imp.html");
+});
+$(".about").click(function(){
+$(".starter-template").load("text/about.html");
+});
+});
